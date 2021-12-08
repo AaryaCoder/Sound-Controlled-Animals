53b126b0156f3a8ba61d7e644636f4ffe02fa2e3
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2021-12-07 22:19:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,39 @@ function modelReady(){
 }
 
 function gotResults(error,results){
-    console.log('Got Result!');
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        random_number_r=Math.floor(Math.random() *255) +1;
+        random_number_g=Math.floor(Math.random() *255) +1;
+        random_number_b=Math.floor(Math.random() *255) +1;
+
+        document.getElementById("times_detected_label").innerHTML='Times Sound Detected - '+ results[0].label;
+        document.getElementById("sound_detected_label").innerHTML='Detected Voice Is Of - '+ results;
+        document.getElementById("times_detected_label").style.color="rgb("+ random_number_r+ " , " + random_number_g+ " , " + random_number_r+ ")";
+        document.getElementById("sound_detected_label").style.color="rgb(" + random_number_r+ " , " + random_number_g+ " , " + random_number_r+ ")";
+
+        if(results[0].label=="Barking"){
+            img.src='barkingdog.jpg';
+            
+        }
+        else if(results[0].label=="Meowing"){
+            img.src='meowcat.jpg';
+            
+        }
+        else if(results[0].label=="Roaring"){
+            img.src='lion.png';
+            
+        }
+        else{
+            img.src='cow.jpg';
+            
+        }
+    }
+    
+}
+
+
 }
